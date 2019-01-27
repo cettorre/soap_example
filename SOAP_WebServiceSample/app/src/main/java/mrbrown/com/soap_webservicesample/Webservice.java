51faf913bf292f3e544aee56645caf183249b80f
Unknown <c.ettorre@gmail.com>
2019-01-27 11:44:01 +0100

    upload soap example

---
+++ SOAP_WebServiceSample/app/src/main/java/mrbrown/com/soap_webservicesample/Webservice.java
@@ -0,0 +1,54 @@
+package mrbrown.com.soap_webservicesample;
+
+import android.content.Context;
+import android.os.StrictMode;
+
+import org.ksoap2.SoapEnvelope;
+import org.ksoap2.serialization.SoapObject;
+import org.ksoap2.serialization.SoapSerializationEnvelope;
+import org.ksoap2.transport.HttpTransportSE;
+
+/**
+ * Created by ufours18 on 4/24/2017.
+ */
+
+class Webservice {
+
+    Context context;
+
+    public Webservice(Context con) {
+        context = con;
+    }
+
+    public String getCountry() {
+
+        try {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+                    .permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+
+            SoapObject request = new SoapObject(context.getResources()
+                    .getString(R.string.namespace), context.getResources()
+                    .getString(R.string.getCountry));
+
+            SoapSerializationEnvelope envelope = new SoapSerializationEnvelope(
+                    SoapEnvelope.VER11);
+            envelope.setOutputSoapObject(request);
+            envelope.dotNet = true;
+            HttpTransportSE androidHttpTransport = new HttpTransportSE(
+                    Constant.Webservice_url);
+            androidHttpTransport.call(
+                    context.getResources().getString(R.string.namespace)
+                            + context.getResources().getString(
+                            R.string.getCountry), envelope);
+
+            String result = envelope.getResponse().toString();
+
+            return result;
+
+        } catch (Exception e) {
+            // TODO: handle exception
+            return e.toString();
+        }
+    }
+}
